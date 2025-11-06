0d6dbfd82dc7b16a02b7e7c79a0940bd0c6559c6
Valeria-Akshina <aksinalera@gmail.com>
2025-11-06 21:47:35 +0300

    section our experience

--- react-one-proects/src/App.js
+++ react-one-proects/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Header } from './component/Header/index.jsx';
 import { Main } from './component/Main/index.jsx';
+import { OurExperience } from './component/OurExperience/index.jsx'
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <Main/>
+      <OurExperience/>
     </div>
   );
 }
